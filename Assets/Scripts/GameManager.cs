c8d6ce0334793fe31364b225d8b9f93ed4bc214e
Sam <sam.huang852@gmail.com>
2021-02-18 02:06:15 +1100

    Basic networking done, UI, rooms join, leave
    was a pain to transfer over but got it working

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    #region Public Fields
+
+    public static GameManager Instance;
+
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
+    
+    private void Awake()
+    {
+        Instance = this;
+        
+    }
+
+    #endregion
+    
+    #region Photon Callbacks
+
+    /// <summary>
+    /// Called when the local player left the room. We need to load the launcher scene.
+    /// </summary>
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", newPlayer.NickName); // not seen if you're the player connecting
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // Called before OnPlayerLeftRoom
+            
+            //LoadArena();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", otherPlayer.NickName); // seen when other disconnects
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // Called before OnPlayerLeftRoom
+            
+            //LoadArena();
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void LeaveRoom()
+    {
+        Debug.Log("leaving room");
+        PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void Start()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+        }
+        else
+        {
+            if (PlayerManager.LocalPlayerInstance == null)
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManager.GetActiveScene().name);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+            }
+            else
+            {
+                Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+            }
+        }
+    }
+
+    private void LoadArena()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+        //PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        PhotonNetwork.LoadLevel("TestScene");
+    }
+
+    #endregion
+}
--- Assets/Scripts/ThirdPersonCameraController.cs
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -8,16 +8,28 @@ public class ThirdPersonCameraController : MonoBehaviour
     [SerializeField] private float frontOffset;
     [SerializeField] private float upOffset;
 
-    void Update()
+    private bool isFollowing;
+
+    void LateUpdate()
     {
-        float targetRotationRadians = target.rotation.eulerAngles.y * Mathf.PI / 180;
+        if (isFollowing)
+        {
 
-        transform.position = new Vector3(
-            target.position.x + frontOffset * Mathf.Sin(targetRotationRadians),
-            target.position.y + upOffset,
-            target.position.z + frontOffset * Mathf.Cos(targetRotationRadians)
-        );
+            float targetRotationRadians = target.rotation.eulerAngles.y * Mathf.PI / 180;
 
-        transform.rotation = Quaternion.LookRotation(target.position - transform.position);
+            transform.position = new Vector3(
+                target.position.x + frontOffset * Mathf.Sin(targetRotationRadians),
+                target.position.y + upOffset,
+                target.position.z + frontOffset * Mathf.Cos(targetRotationRadians)
+            );
+
+            transform.rotation = Quaternion.LookRotation(target.position - transform.position);
+        }
+    }
+
+    public void OnStartFollowing(Transform _target)
+    {
+        isFollowing = true;
+        target = _target;
     }
 }
