c97f90a587f1675a26c6077e10997ada597c7c34
Jack Coggins <voidawake90@gmail.com>
2021-02-22 20:48:00 +1100

    Add explosion

--- Assets/Scripts/PowerupManager.cs
+++ Assets/Scripts/PowerupManager.cs
@@ -71,7 +71,7 @@ public class PowerupManager : MonoBehaviour
                 transform.rotation
             );
 
-            //currentPowerup = null;
+            currentPowerup = null;
         }
     }
 
